836732eb770c2a7a4aa6c1f3e27e7c7e849643b1
Anton Mikhalev <DzhoyNik@yandex.ru>
2026-03-17 01:26:43 +0700

    feat: add a layout profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,54 @@
-import { React } from "react"
+import { NavLink } from "react-router"
+import Footer from "../../components/Footer"
+import Overlay from "../../components/Overlay"
+import styles from "./profile.module.css"
 
 const Profile = () => {
     return(
         <>
-            <h1>PROFILE!</h1>
+            <Overlay />
+            <div className={styles.profile}>
+                <div className={styles.profile__content}>
+                    <div className={styles.profile__sidebar}>
+                        <div className={styles.profile__sidebarSection}>
+                            <button type="button">Решать кейсы</button>
+                        </div>
+                        <div className={styles.profile__sidebarSection} style={{ padding: "1rem" }}>
+                            <div className={styles.profile__sidebarSectionItem}>
+                                <NavLink to="">Профиль</NavLink>
+                            </div>
+                            <div className={styles.profile__sidebarSectionItem}>
+                                <NavLink to="">Уведомления</NavLink>
+                            </div>
+                            {/* <div className={styles.profile__sidebarSectionItem}>
+                                <NavLink to="">Мои кейсы</NavLink>
+                            </div> */}
+                        </div>
+                        <div className={styles.profile__sidebarSection} style={{ padding: "1rem" }}>
+                            <div className={styles.profile__sidebarSectionItem}>
+                                <NavLink to="">Портфолио</NavLink>
+                            </div>
+                            <div className={styles.profile__sidebarSectionItem}>
+                                <NavLink to="">Навыки</NavLink>
+                            </div>
+                        </div>
+                        <div className={styles.profile__sidebarSection} style={{ padding: "1rem" }}>
+                            <div className={styles.profile__sidebarSectionItem}>
+                                <NavLink to="">Настройки</NavLink>
+                            </div>
+                            <div className={styles.profile__sidebarSectionItem}>
+                                <NavLink to="">Выйти</NavLink>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.profile__main}>
+                        <div className={styles.profile__mainSection}></div>
+                        <div className={styles.profile__mainSection}></div>
+                        <div className={styles.profile__mainSection}></div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </>
     )
 }
