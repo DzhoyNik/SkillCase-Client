f0d173cc0329faae4460798d6bfb8cb99ecb1091
Anton Mikhalev <DzhoyNik@yandex.ru>
2026-03-19 16:04:35 +0700

    feat: update a cases page

--- src/pages/cases/CaseItem.jsx
+++ src/pages/cases/CaseItem.jsx
@@ -2,7 +2,27 @@ import styles from "./cases.module.css"
 
 const CaseItem = () => {
     return(
-        <div className={styles.case__item}></div>
+        <div className={styles.case__item}>
+            <div className={styles.case__difficulty}>
+                <h4>Легко</h4>
+                <div className={styles.case__difficultyProgressBar}>
+                    <div className={styles.case__difficultyProgress} />
+                </div>
+                <h4>Сложно</h4>
+            </div>
+            <h3 className={styles.case__title}>Кейс от компании «Яндекс»</h3>
+            <p className={styles.case__description}>Реши кейс от компании «Яндекс» и получи возможность пройти стажировку в одной из лучших IT-компаний России.</p>
+            <div className={styles.case__tags}>
+                <h4 className={styles.case__tag}>Frontend</h4>
+                <h4 className={styles.case__tag}>Backend</h4>
+                <h4 className={styles.case__tag}>Frontend</h4>
+                <h4 className={styles.case__tag}>Backend</h4>
+            </div>
+            <div className={styles.case__author}>
+                <div className={styles.case__authorIcon}></div>
+                <p className={styles.case__authorText}>Author || Company</p>
+            </div>
+        </div>
     )
 }
 
--- src/pages/cases/Cases.jsx
+++ src/pages/cases/Cases.jsx
@@ -2,7 +2,7 @@ import styles from "./cases.module.css"
 import footer from "../landing/landing.module.css"
 import Overlay from "../../components/Overlay"
 import CaseItem from "./CaseItem"
-import { IoChevronBack, IoChevronForward } from "react-icons/io5"
+import { IoChevronBack, IoChevronForward, IoSearch } from "react-icons/io5"
 import Footer from "../../components/Footer"
 
 const Cases = () => {
@@ -13,21 +13,36 @@ const Cases = () => {
                 <h2 className={styles.cases__title}>Каталог кейсов</h2>
                 <p className={styles.cases__description}>Выбирай практические задачи от компаний, решай в своём темпе и добавляй результат в портфолио.</p>
                 <div className={styles.cases__content}>
-                    <div className={styles.cases__sidebar}></div>
+                    <div className={styles.cases__sidebar}>
+                        <div className={styles.cases__sidebarSection}>
+                            <IoChevronForward />
+                            <h3>Уровень сложности</h3> 
+                        </div>
+                        <div className={styles.cases__sidebarSection}>
+                            <IoChevronForward />
+                            <h3>Навыки</h3>
+                        </div>
+                        <div className={styles.cases__sidebarSection}>
+                            <IoChevronForward />
+                            <h3>Направления</h3>
+                        </div>
+                        <button type="button">Применить фильтры</button>
+                        <button type="button">Сбросить</button>
+                    </div>
                     <div className={styles.cases__main}>
-                        <div className={styles.cases__search}></div>
+                        <div className={styles.cases__toolbar}>
+                            <div className={styles.cases__search}>
+                                <input type="text" placeholder="Поиск по кейсам" />
+                                <button type="button">
+                                    <IoSearch />
+                                </button>
+                            </div>
+                            <div className={styles.cases__sort}>
+                                <h3>Сортировать</h3>
+                                <IoChevronBack />
+                            </div>
+                        </div>
                         <div className={styles.cases__list}>
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
-                            <CaseItem />
                             <CaseItem />
                         </div>
                     </div>
